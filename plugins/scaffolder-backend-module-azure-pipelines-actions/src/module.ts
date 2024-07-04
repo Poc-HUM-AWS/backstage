04d247624f35ee79533d6f3e367d140d63ea79cf
jrboliva_bancol <jrboliva@bancolombia.com.co>
2024-07-04 16:14:52 -0500

    se agrega template con action para pipeline

---
+++ plugins/scaffolder-backend-module-azure-pipelines-actions/src/module.ts
@@ -0,0 +1,27 @@
+import {
+  coreServices,
+  createBackendModule,
+} from '@backstage/backend-plugin-api';
+
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+
+import {createAzurePipelineAction} from './actions/createAzurePipeline'
+import {runAzurePipelineAction} from './actions/runAzurePipeline'
+
+export const scaffolderModuleAzurePipelinesActions = createBackendModule({
+  pluginId: 'scaffolder',
+  moduleId: 'azure-pipelines-actions',
+  register(reg) {
+    reg.registerInit({
+      deps: {
+        scaffolder: scaffolderActionsExtensionPoint,
+        config: coreServices.rootConfig,
+        // ... and other dependencies as needed
+      },
+      async init({ scaffolder, config }) {
+        scaffolder.addActions(createAzurePipelineAction({ config }));
+        scaffolder.addActions(runAzurePipelineAction({ config }));
+      },
+    });
+  },
+});
