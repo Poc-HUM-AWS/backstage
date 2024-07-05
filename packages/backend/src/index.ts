6323b9e7324713ff97d47656846d0ddaf8124e60
jrboliva_bancol <jrboliva@bancolombia.com.co>
2024-07-05 09:33:50 -0500

    se crea action ECR

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -45,6 +45,7 @@ backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-azure'));
 //actions
 backend.add(scaffolderModuleAzurePipelinesActions);
+backend.add(import('@aws/aws-core-plugin-for-backstage-scaffolder-actions'));
 
 // humanitec
 backend.add(import('@humanitec/backstage-plugin-backend'));
